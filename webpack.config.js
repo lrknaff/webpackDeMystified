03806c81f6d8c1d6cb79e142ee628a90ac8aa9bb
lrknaff <lacey.knaff@gmail.com>
2016-10-04 10:09:14 -0600

    added bable dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -8,25 +8,15 @@ module.exports = {
   output: {
     path: __dirname,
     filename: "[name].bundle.js"
-    //<script> main/test.bundle.js
-  }
-}
-
-const path = require('path');
-
-module.exports = {
-  entry: {
-    main: "./lib/index.js",
-    test: "mocha!./test/index.js"
-  },
-  output: {
-    path: __dirname,
-    filename: "[name].bundle.js"
   },
   module: {
     loaders: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!sass" }
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
   }
 }
