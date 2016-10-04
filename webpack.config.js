7551678794f2387f89a1d6a2e162a36f67011c70
lrknaff <lacey.knaff@gmail.com>
2016-10-04 10:05:23 -0600

    added and installed sass and sass file

--- webpack.config.js
+++ webpack.config.js
@@ -11,3 +11,22 @@ module.exports = {
     //<script> main/test.bundle.js
   }
 }
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  }
+}
